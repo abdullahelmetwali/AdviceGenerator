87d8d0dfaff7f36839466ff3d54bce316cd3306f
Abdullah ElMetwali <133114413+abdullahelmetwali@users.noreply.github.com>
2024-01-21 17:43:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+import advices from "./advices.json" assert {type:"json"}
+let theDice = document.querySelector(".dice")
+let theAdvice = document.querySelector(".advice")
+let theId = document.querySelector(".id")
+
+function randomAdviceNumber(min,max){
+    return Math.floor(Math.random() * (max - min) + 0);
+}
+
+theDice.addEventListener("click", () =>{
+    var allAdvices = Object.entries(advices)[randomAdviceNumber(0,103)]
+    theId.innerHTML = allAdvices[1].adviceId
+    theAdvice.innerHTML = `“${allAdvices[1].adviceContent}”` 
+})
+
+
+
+
+
