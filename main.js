bd3f75d937283628400ba7e888742d802c0c2c04
Abdullah ElMetwali <133114413+abdullahelmetwali@users.noreply.github.com>
2024-01-22 03:35:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ function randomAdviceNumber(min,max){
     return Math.floor(Math.random() * (max - min) + 0);
 }
 
-theDice.addEventListener("click touchstart", () =>{
+theDice.addEventListener("click", () =>{
+    var allAdvices = Object.entries(advices)[randomAdviceNumber(0,103)]
+    theId.innerHTML = allAdvices[1].adviceId
+    theAdvice.innerHTML = `“${allAdvices[1].adviceContent}”` 
+})
+theDice.addEventListener("touchstart",()=>{
     var allAdvices = Object.entries(advices)[randomAdviceNumber(0,103)]
     theId.innerHTML = allAdvices[1].adviceId
     theAdvice.innerHTML = `“${allAdvices[1].adviceContent}”` 
