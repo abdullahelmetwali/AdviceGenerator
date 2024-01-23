4c6ecae94effc9b890062520fa9760a6e9134c1d
Abdullah ElMetwali <133114413+abdullahelmetwali@users.noreply.github.com>
2024-01-23 21:55:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,23 +2,15 @@ import advices from "./advices.json" assert {type:"json"}
 let theDice = document.querySelector(".dice")
 let theAdvice = document.querySelector(".advice")
 let theId = document.querySelector(".id")
+// For PC and Mobile 
+let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
 function randomAdviceNumber(min,max){
     return Math.floor(Math.random() * (max - min) + 0);
 }
 
-theDice.addEventListener("click", () =>{
+theDice.addEventListener(touchEvent, () =>{
     var allAdvices = Object.entries(advices)[randomAdviceNumber(0,103)]
     theId.innerHTML = allAdvices[1].adviceId
     theAdvice.innerHTML = `“${allAdvices[1].adviceContent}”` 
 })
-theDice.addEventListener("touchstart",()=>{
-    var allAdvices = Object.entries(advices)[randomAdviceNumber(0,103)]
-    theId.innerHTML = allAdvices[1].adviceId
-    theAdvice.innerHTML = `“${allAdvices[1].adviceContent}”` 
-})
-
-
-
-
-
